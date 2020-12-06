fccf676fa4418b7ee654bd511c3c7d6142a9b460
Terrell Mack <Terrell.Mack@teachforamerica.org>
2020-12-06 13:05:51 -0500

    Code Cleanup

---
+++ README.md
@@ -0,0 +1,14 @@
+# R Project Template
+This is a template for creating R Projects
+
+Example Usage
+``` bash
+Rscript say-hello.R --name "World", --hello-count 5 --output-path data/hello.txt
+```
+
+This Should create a file names "data/hello.txt" with the following contents:
+```text
+
+```
+
+
--- say-hello.R
+++ say-hello.R
@@ -26,16 +26,18 @@ if (!is.null(opt['output-path']) ) {
     output_path <- trimws(opt['output-path'])
 }
 
+print(name)
 # create the output path if it doesn't exist
 output_dir <- dirname(output_path)
 results <- ifelse(!dir.exists(file.path(output_dir)), dir.create(file.path(output_dir), recursive=TRUE), FALSE)
 results <- file.create(output_path)
 
 # now lets open the file and write to it
-file_connection<-file(output_path)
+file_connection<-file(output_path, open = "wt")
+hello_to_write <- paste(paste("Hello", name, sep=" "), '!', sep="")
 for (i in 1:hello_count) {
-  hello_to_write = paste(paste("Hello", name, sep=" "), '!', sep="")
   print(paste("Writing", hello_to_write, "to", output_path, sep=" "))
   writeLines(hello_to_write, file_connection)
+
 }
 close(file_connection)
